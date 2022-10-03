b9ab256d907dfee23b0205bed27c45f9943aa8ea
leoniephelps <leoniephelps96@gmail.com>
2022-10-03 15:23:58 +0100

    initialise project section

---
+++ src/components/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Projects = () => {
+    return (
+      <div class="mt-20 bg-white flex  flex-col">
+        <h1 class="font-workSans font-bold text-4xl flex justify-center mt-10">
+          My recent work
+        </h1>
+        <h2 class="font-workSans font-normal flex justify-center">
+          I love creating projects lol come and have a look at some of the ones
+          I've made below
+        </h2>
+      </div>
+    );
+}
