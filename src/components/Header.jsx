38840dfc2e4347cd788c49ccfee2be703f6c1ae5
leoniephelps <leoniephelps96@gmail.com>
2022-10-03 20:54:23 +0100

    add flex

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div class="text-white flex flex-col mt-20">
-      <h1 class="text-7xl font-workSans font-extrabold flex justify-center">
+    <div class="text-white flex-col items-center mt-20 pl-3 pr-3">
+      <h1 class="text-7xl font-workSans font-extrabold text-center">
         Hi, I'm Leonie
       </h1>
-      <h2 class="font-workSans font-normal flex justify-center">
+      <h2 class="font-workSans font-normal text-center mt-10">
         I decided to become a software developer because I love creative problem
         solving, and am excited to further grow my skills in existing and new
         technologies. Most recently I have trained in full-stack software
@@ -14,7 +14,7 @@ export const Header = () => {
         my peers on small sprints and working on my solo and group projects,
         demonstrating my skills as an adaptable learner.
       </h2>
-      <h2 class="font-workSans font-normal flex justify-center">
+      <h2 class="font-workSans font-normal flex justify-center text-center">
         Previously I worked as a search engine evaluator, where I contributed to
         improving the world’s leading search engine by providing feedback about
         the accuracy and quality of text and video search results.
