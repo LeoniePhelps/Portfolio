2328d7d4a1c59de6c08da2db7ffc4fd78b7ecac4
leoniephelps <leoniephelps96@gmail.com>
2022-10-03 14:44:14 +0100

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div class="text-white flex justify-centre">
+      <h1 class="text-2xl font-workSans font-extrabold">Hi, I'm Leonie</h1>
+    </div>
+  );
+};
+//font-family: 'Work Sans', sans-serif;
