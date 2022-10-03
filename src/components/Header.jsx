08dc56df2f7d010177f36d943ca28193d5ed4543
leoniephelps <leoniephelps96@gmail.com>
2022-10-03 15:24:06 +0100

    add flex

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,23 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div class="text-white flex justify-centre">
-      <h1 class="text-2xl font-workSans font-extrabold">Hi, I'm Leonie</h1>
+    <div class="text-white flex flex-col mt-20">
+      <h1 class="text-7xl font-workSans font-extrabold flex justify-center">
+        Hi, I'm Leonie
+      </h1>
+      <h2 class="font-workSans font-normal flex justify-center">
+        I decided to become a software developer because I love creative problem
+        solving, and am excited to further grow my skills in existing and new
+        technologies. Most recently I have trained in full-stack software
+        development at Northcoders. I had an amazing time pair- programming with
+        my peers on small sprints and working on my solo and group projects,
+        demonstrating my skills as an adaptable learner.
+      </h2>
+      <h2 class="font-workSans font-normal flex justify-center">
+        Previously I worked as a search engine evaluator, where I contributed to
+        improving the world’s leading search engine by providing feedback about
+        the accuracy and quality of text and video search results.
+      </h2>
     </div>
   );
 };
-//font-family: 'Work Sans', sans-serif;
