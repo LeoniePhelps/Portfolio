d083cd4dd6819f2bcdfbd152f0e0f315dec91786
leoniephelps <leoniephelps96@gmail.com>
2022-10-03 19:19:30 +0100

    add flex to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export const NavBar = () => {
   return (
     <div class="text-white flex flex-row justify-center font-workSans font-medium mt-10">
-      <h1 class="basis-1/6">About</h1>
-      <h1 class="basis-1/6">Experience</h1>
-      <h1 class="basis-1/6">Projects</h1>
-      <h1 class="basis-1/6">Contact</h1>
+      <h1 class="mr-2 md:mr-20">About</h1>
+      <h1 class="mr-2 md:mr-20">Experience</h1>
+      <h1 class="mr-2 md:mr-20">Projects</h1>
+      <h1 class="">Contact</h1>
     </div>
   );
 };
