54b522fe6962cc9462f03a228854c7b5085cbac7
leoniephelps <leoniephelps96@gmail.com>
2022-10-03 14:44:19 +0100

    add navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <div class="text-white flex flex-row justify-center font-workSans font-medium mt-10">
+      <h1 class="basis-1/6">About</h1>
+      <h1 class="basis-1/6">Experience</h1>
+      <h1 class="basis-1/6">Projects</h1>
+      <h1 class="basis-1/6">Contact</h1>
+    </div>
+  );
+};
+
+
