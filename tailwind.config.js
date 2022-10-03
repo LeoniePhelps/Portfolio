69aad64198c9748122a2b37ffa0dca677404c491
leoniephelps <leoniephelps96@gmail.com>
2022-10-03 14:43:15 +0100

    add font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      workSans: ["Work Sans", "sans-serif"],
+    },
+    fontWeight: {
+      normal: 400,
+      medium: 500,
+      bold: 700,
+      extrabold: 800,
+    },
   },
   plugins: [],
 };
