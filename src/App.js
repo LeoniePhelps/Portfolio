dddd48f22fc81e386773a4237a328ecba0236cd6
leoniephelps <leoniephelps96@gmail.com>
2022-10-03 15:23:48 +0100

    add projects section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { NavBar } from "./components/NavBar";
 import { Header } from "./components/Header";
+import { Projects } from "./components/Projects";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Header />
+      <Projects />
     </div>
   );
 }
