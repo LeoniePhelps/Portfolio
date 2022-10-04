c4ced3f11c5a41f4653f412103c8413e2170a70b
leoniephelps <leoniephelps96@gmail.com>
2022-10-04 17:39:50 +0100

    add contact

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { NavBar } from "./components/NavBar";
 import { Header } from "./components/Header";
 import { Projects } from "./components/Projects";
-import { Skills } from "./components/Skills";
+import { Experience } from "./components/Experience";
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <NavBar />
       <Header />
       <Projects />
-      <Skills />
+      <Experience />
+      <Contact />
     </>
   );
 }
