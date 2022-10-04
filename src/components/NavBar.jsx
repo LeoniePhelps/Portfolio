b484e544816f3764770296c496cbbafbcd0df848
leoniephelps <leoniephelps96@gmail.com>
2022-10-04 12:25:06 +0100

    change order of links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,11 +4,9 @@ export const NavBar = () => {
   return (
     <div class="text-white flex flex-row justify-center font-workSans font-medium mt-10">
       <h1 class="mr-2 md:mr-20">About</h1>
-      <h1 class="mr-2 md:mr-20">Experience</h1>
       <h1 class="mr-2 md:mr-20">Projects</h1>
+      <h1 class="mr-2 md:mr-20">Skills</h1>
       <h1 class="">Contact</h1>
     </div>
   );
 };
-
-
