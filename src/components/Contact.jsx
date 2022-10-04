c957b8b4e1aa3c926f3a18ff34cdff3eabf722f0
leoniephelps <leoniephelps96@gmail.com>
2022-10-04 17:40:45 +0100

    initialise contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Contact = () => {
+  return (
+    <div class="text-white flex-col items-center mt-20 pl-40 pr-40 mb-20">
+      <h1 class="text-6xl font-workSans font-extrabold text-center">
+        Contact me
+      </h1>
+      <a href={`mailto:leoniehhphelps@gmail.com`}>leoniehhphelps@gmail.com</a>
+      <p class="font-workSans">Leonie Phelps 2022</p>
+    </div>
+  );
+};
