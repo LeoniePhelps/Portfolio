bd8e5e1a7ddf26d5b1cc011a3ae701557fe68ab1
leoniephelps <leoniephelps96@gmail.com>
2022-10-06 20:34:33 +0100

    import CSS

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { Projects } from "./components/Projects";
 import { Experience } from "./components/Experience";
 import { Contact } from "./components/Contact";
+import "./App.css";
 
 function App() {
   return (
