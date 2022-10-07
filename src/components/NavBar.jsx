891cbde80c7a25ac5872bb296378acdd9fe43d53
leoniephelps <leoniephelps96@gmail.com>
2022-10-07 20:40:06 +0100

    change spacing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export const NavBar = () => {
   return (
-    <header class="text-white flex flex-row justify-center mt-5 font-workSans font-medium ">
+    <header class="text-white flex flex-row justify-center mt-5 font-workSans font-medium">
       <button
         onClick={() => window.location.replace("/#projects")}
-        class="mr-2 md:mr-20"
+        class="mr-5 md:mr-48"
       >
         Projects
       </button>
       <button
         onClick={() => window.location.replace("/#experience")}
-        class="mr-2 md:mr-20"
+        class="mr-5 md:mr-48"
       >
         Experience
       </button>
