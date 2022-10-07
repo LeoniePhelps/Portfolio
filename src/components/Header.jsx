cb68b838d7d4ab84b816aa522622b2ca0583add9
leoniephelps <leoniephelps96@gmail.com>
2022-10-07 20:39:49 +0100

    edit about section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,28 @@ import React from "react";
 export const Header = () => {
   return (
     <div class="text-white flex-col items-center mt-28 pl-5 pr-5 md:pl-40 md:pr-40">
-      <h1 class="text-7xl font-workSans font-extrabold text-center">
+      <h1
+        id="title"
+        class="text-7xl md:text-8xl font-workSans font-extrabold text-center"
+      >
         Hi, I'm Leonie
       </h1>
 
-      <h2 class="font-workSans text-center mt-20">
-        I decided to become a software developer because I love creative problem
-        solving, and am excited to further grow my skills in existing and new
-        technologies. Most recently I have trained in full-stack software
-        development at Northcoders. I had an amazing time pair- programming with
-        my peers on small sprints and working on my solo and group projects,
-        demonstrating my skills as an adaptable learner.
+      <h2 class="md:mt-40 mt-28 font-workSans font-bold text-center text-[20px]">
+        Software developer with a passion for problem solving and a desire to
+        create.
       </h2>
-      <h2 class="font-workSans flex justify-center text-center pb-20">
-        Previously I worked as a search engine evaluator, where I contributed to
-        improving the world’s leading search engine by providing feedback about
-        the accuracy and quality of text and video search results.
+
+      <h2 class="font-workSans text-center mt-5">
+        I decided to become a software developer because I love creating useful
+        products that enrich peoples lives. Most recently, I have had an amazing
+        time training in full-stack software development at Northcoders and am
+        motivated to further my skills in existing and new technologies.
+      </h2>
+      <h2 class="font-workSans flex justify-center text-center pb-20 mt-3">
+        Previously I worked as a Search Engine Evaluator, where I contributed to
+        improving the world’s leading search engine with the aim of improving
+        user experience and training machine learning algorithms.
       </h2>
     </div>
   );
